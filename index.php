c6bcec5fdad9b2d7cb0611b44cac8fdfed5f2411
Luke McKennan <luke.mckennan@gmail.com>
2015-12-03 10:54:58 +0800

    testing the waters, basic html

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
 <html>
 <?php include('partials/head.html'); ?>
-<body>
-hello world
+<body ng-app>
+	<div ng-controller="ToDoCtrl">
+		<h1>There's still XXX things left to do</h1>
+
+		<ul>
+			<li></li>
+		</ul>
+
+		<form>
+			<input placeholder="search for stuff here" type="text" ng-model="formText" />
+			<button class="addMore" ng-click="addMore">Add To Do!</button>
+		</form>
+	</div>
 </body>
 </html>

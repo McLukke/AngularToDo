38ce61be03efab0116dbfd49bd37101410cb7d96
Luke McKennan <luke.mckennan@gmail.com>
2015-12-17 12:15:53 +0800

    change to object array containing 'done' status / new model to change 'done' status / add strikethrough style / add ng-show status for a-tag

--- dist/all.js
+++ dist/all.js
@@ -2,9 +2,51 @@
 var todoApp = angular.module('todoApp', []);
 
 todoApp.controller('todoCtrl', ['$scope', function($scope) {
-	$scope.tasks = ['brush teeth', 'comb hair', 'fold bedsheets'];
+	$scope.tasks = [
+		{
+			text: 'brush teeth',
+			done: false
+		},
+		{
+			text: 'comb hair',
+			done: false
+		},
+		{
+			text: 'fold bedsheets',
+			done: false
+		}
+	];
+
+	$scope.todoTotal = function() {
+		var uncompletedTasks = 0;
+
+		$scope.tasks.forEach(function(currentElement, index, array) {
+			// console.log(currentElement);
+			// console.log(index);
+			// console.log(array);
+
+			if (currentElement.done === false) uncompletedTasks += 1;
+		});
+		return uncompletedTasks;
+	};
 
 	$scope.addTask = function() {
-		
-	}
+		if ($scope.newTask === "" || $scope.newTask === undefined) {
+			console.log('please enter a value');
+		} else {
+			$scope.tasks.push(
+				{
+					text: $scope.newTask,
+					done: false
+				}
+			);
+			$scope.newTask = "";
+		}
+	};
+
+	$scope.toggleComplete = function(finishedTask) {
+		console.log(finishedTask.done);
+
+		finishedTask.done = !finishedTask.done;
+	};
 }]);
--- index.php
+++ index.php
@@ -2,16 +2,37 @@
 <?php include('partials/head.html'); ?>
 <body ng-app="todoApp">
 	<div ng-controller="todoCtrl">
-		<h1>There's still XXX things left to do</h1>
-
+		<h1>There's still {{todoTotal()}} things left to do</h1>
+		<br />
 		<ul>
-			<li ng-repeat="task in tasks">{{task}}</li>
+			<li ng-repeat="task in tasks">
+				<span class="taskCompleted-{{task.done}}">{{task.text}}</span>
+				<a href="#" ng-click="toggleComplete(task)">
+					<span ng-show="task.done">Not Finished Yet...</span>
+					<span ng-show="!task.done">Finished!</span>
+				</a>
+				<hr>
+			</li>
 		</ul>
 
 		<form ng-submit="addTask()">
 			<input placeholder="Add a new to-do" type="text" ng-model="newTask" />
 			<input type="submit" value="Add to-do!">
 		</form>
+		{{newTask}}
 	</div>
+<style>
+	span > a {
+		float: right;
+	}
+
+	.taskCompleted-true {
+		text-decoration: line-through;
+	}
+
+	.taskCompleted-false {
+		text-decoration: none;
+	}
+</style>
 </body>
 </html>
